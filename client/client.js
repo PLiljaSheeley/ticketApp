32ae8f99b0b66e535941cdb3ded909a49ad68ede
Peter Lilja-Sheeley <pliljasheeley@gmail.com>
2016-04-18 12:41:57 -0500

    working edit function through remove then add, needs to be changed to editing the specific ticket and needs a date update and styling

--- client/client.js
+++ client/client.js
@@ -3,18 +3,26 @@ var app = angular.module("app", []);
 app.controller("MainController", ["$scope", "$http", function($scope, $http) {
   $scope.ticketArray = [];
   $scope.ticket = {};
-  $scope.show = false;
-
+  $scope.edit = false;
+  $scope.toggle = true;
+  $scope.count = 0;
   $scope.getTickets = function() {
     $http.get("/ticket").then(function(response) {
       $scope.ticket = {};
       $scope.ticketArray = response.data;
-    }); //  $http.get
-    $scope.show = true
+    });
   };
   $scope.getTickets();
   $scope.add = function(ticket){
     $http.post('/add', ticket).then($scope.getTickets());
+  }
+  $scope.change = function(ticket){
+    $scope.count++;
+    if($scope.count%2===0){
+    $http.post('/add', ticket);
+  }else if($scope.count%2 !== 0){
+    $http.delete("/deleteTicket/" + ticket._id);
+  }
   }
   $scope.deleteTicket = function(ticket) {
     $http.delete("/deleteTicket/" + ticket._id).then(function(response) {
